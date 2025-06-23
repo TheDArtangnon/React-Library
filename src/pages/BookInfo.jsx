96d52061a3d03cd8303e5e1a3b8e8c8389eec376
DArtangnon <dartangnon101@hotmail.com>
2025-06-22 17:11:37 -0700

    Recommended Books Update

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -1,10 +1,16 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import Rating from '../components/ui/Rating'
 import Price from '../components/ui/Price'
+import Book from '../components/ui/Book'
 
 export default function BookInfo({ books }) {
+  const { id } = useParams()
+const book = books.find(book => +book.id === +id)
+console.log(book)
+
+  console.log(id)
   return (
     <div id="books__body">
       <main id="books__main">
@@ -20,18 +26,46 @@ export default function BookInfo({ books }) {
                 </div>
                 <div className="book__selected">
                   <figure className="book__selected--figure">
-                    <img src="https://m.media-amazon.com/images/I/61mIq2iJUXL._AC_UF1000,1000_QL80_.jpg" alt="" className="book__selected--img" />
+                    <img src={book.url} alt="" className="book__selected--img" />
                   </figure>
                   <div className="book__selected--description">
-                    <h2 className="book__selected--title">Crack the coding interview</h2>
-                    <Rating rating="4.5" />
+                    <h2 className="book__selected--title">{book.title}</h2>
+                    <Rating rating={book.rating}/>
                     <div className="book__selected--price">
-                    <Price originalPrice={50} salePrice={20} />
+                    <Price originalPrice={book.originalPrice} salePrice={book.salePrice} />
                     </div>
-                    <div className="book__summary"></div>
+                    <div className="book__summary">
+                      <h3 className="book__summary--title">Summary</h3>
+                      <p className="book__summary--para">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima eius cumque illum, corrupti quia cupiditate reprehenderit aspernatur officia eveniet at quos magni rerum nobis porro animi eum distinctio! Amet, distinctio.
+                      </p>
+                      <p className="book__summary--para">
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima eius cumque illum, corrupti quia cupiditate reprehenderit aspernatur officia eveniet at quos magni rerum nobis porro animi eum distinctio! Amet, distinctio.
+                      </p>
+                    </div>
+                    <button className="btn">
+                      Add To Cart
+                    </button>
                   </div>
                 </div>
             </div>
+        </div>
+
+        <div className="books__container">
+          <div className="row">
+            <div className="book__selected--top">
+              <h2 className="book__selected--title--top">
+                Recommended Books
+              </h2>
+            </div>
+            <div className="books">
+             {books
+              .filter((book) => book.rating === 5 && +book.id !== +id)
+              .slice(0, 4)
+              .map(book => <Book book={book} key={book.id} />)
+             }
+             </div>
+          </div>
         </div>
       </main>
     </div>
