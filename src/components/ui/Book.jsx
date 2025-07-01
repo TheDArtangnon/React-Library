24aa6ac68dc61cbfc5e3d594ca67a0b45bd177c0
DArtangnon <dartangnon101@hotmail.com>
2025-07-01 01:24:50 -0700

    Loading state intro

--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -1,27 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import Rating from './Rating'
 import Price from './Price'
 
 export default function Book({ book }) {
+  const [img, setImg] = useState ()
 
+useEffect(() => {
+  const image = new Image()
+  image.src = book.url
+  image.onload = () => {
+    setImg(image)
+  }
+})
+  
   return (
   <div className="book">
-            <Link to={`/books/${book.id}`}>
+     {img ? (
+      <>
+     <Link to={`/books/${book.id}`}>
               <figure className="book__img--wrapper">
-                <img src={book.url}
+                <img src={img.src}
                 alt=""
-                className="book__img"
+                className="book__img" 
                 />
               </figure>
             </Link>
             <div className="book__title">
-              <Link to={`/books/${book.id}`} className="book__title--link">
+              <Link to={`/books/${book.id}`} className="book__title--link" /> 
               {book.title}
               </Link>
             </div>
             <Rating rating={book.rating} />
-           <Price salePrice={book.salePrice} originalPrice={book.originalPrice} />
-          </div>
+           <Price salePrice={book.salePrice} originalPrice={book.originalPrice} /> 
+           </> 
+     ) : (
+      <> 
+      <div className="book__img--skeleton"></div>
+    <div className="skeleton book__title--skeleton"></div>
+    <div className="skeleton book__rating--skeleton"></div>
+    <div className="skeleton book__price--skeleton"></div>
+      </>
+    )}
+     </div>
   )
 }
